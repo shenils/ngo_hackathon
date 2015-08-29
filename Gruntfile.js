00ae198dc5311003cd8afddc15fc7f4fe8cd1f30
shenils <shenil@bu.edu>
2015-08-29 16:07:43 +0530

    website

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {                              // Task 
+      dist: {                            // Target 
+        files: {                         // Dictionary of files 
+          'assets/stylesheets/common.css': 'assets/stylesheets/src/common.scss',
+          'assets/stylesheets/land.css': 'assets/stylesheets/src/land.scss',
+          'assets/stylesheets/about.css': 'assets/stylesheets/src/about.scss',
+          // 'assets/stylesheets/how_works.css': 'assets/stylesheets/src/how_works.scss',
+          // 'assets/stylesheets/services.css': 'assets/stylesheets/src/services.scss',
+          // 'assets/stylesheets/contact.css': 'assets/stylesheets/src/contact.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['assets/stylesheets/src/*.scss'],
+        tasks: ['sass', 'cssmin'],
+      }
+    },
+    // concat: {
+    //   css: {
+    //     src: ['assets/stylesheets/common.css', 'assets/stylesheets/land.css','assets/stylesheets/about.css'],
+    //     dest: 'assets/stylesheets/app.css',
+    //   },
+    // }
+    cssmin: {
+      target: {
+        files: {
+          'assets/stylesheets/app.min.css': 
+            [
+              'assets/stylesheets/common.css', 
+              'assets/stylesheets/land.css',
+              'assets/stylesheets/about.css',
+              // 'assets/stylesheets/how_works.css',
+              // 'assets/stylesheets/services.css',
+              // 'assets/stylesheets/contact.css'
+            ]
+        }
+      }
+    }
+  });
+
+
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  //.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+psd2html
+
+Building a static webpage that looks like http://blazrobar.com/2015/free-psd-website-templates/pex-a-free-website-home-page-photoshop-psd/
